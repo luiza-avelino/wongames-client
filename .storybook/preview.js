5a75b5d59d9550340bab582eac24b83065449aca
Luiza Avelino <avelino.luiza98@gmail.com>
2022-04-22 17:16:16 -0300

    fix styles import at storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from '../src/styles/global'
+import GlobalStyles from 'styles/global'
 
-import theme from '../src/styles/theme'
+import theme from 'styles/theme'
 
 export const parameters = {
   nextRouter: {
