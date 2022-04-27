13ffc7e868b17c8b6ca5d86d61aa805b66874b53
Luiza Avelino <avelino.luiza98@gmail.com>
2022-04-27 15:42:32 -0300

    add styles to MenuFull

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -39,12 +39,111 @@ export const MenuGroup = styled.div`
   `}
 `
 
+export const MenuNav = styled.div``
+
+export const MenuLink = styled.a`
+  ${({ theme }) => css`
+    position: relative;
+    color: ${theme.colors.black};
+    font-size: ${theme.font.sizes.medium};
+    font-weight: ${theme.font.bold};
+    text-decoration: none;
+    text-align: center;
+    margin: 0.3rem ${theme.spacings.small} 0;
+
+    &:hover {
+      &::after {
+        content: '';
+        position: absolute;
+        display: block;
+        height: 0.3rem;
+        background: ${theme.colors.primary};
+        animation: hoverAnimation 0.2s forwards;
+      }
+
+      @keyframes hoverAnimation {
+        from {
+          width: 0;
+          left: 50%;
+        }
+
+        to {
+          width: 100%;
+          left: 0;
+        }
+      }
+    }
+  `}
+`
+
 type MenuFullProps = {
   isOpen: boolean
 }
 
 export const MenuFull = styled.nav<MenuFullProps>`
-  ${({ isOpen }) => css`
+  ${({ theme, isOpen }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
     opacity: ${isOpen ? 1 : 0};
+    background: ${theme.colors.white};
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    overflow: hidden;
+    height: 100vh;
+    pointer-events: ${isOpen ? 'all' : 'none'};
+
+    > svg {
+      cursor: pointer;
+      width: 2.4rem;
+      height: 2.4rem;
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin: ${theme.spacings.xsmall};
+    }
+
+    ${MenuNav} {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      flex: 1;
+    }
+
+    ${MenuLink} {
+      color: ${theme.colors.black};
+      font-weight: ${theme.font.bold};
+      font-size: ${theme.font.sizes.xlarge};
+      margin-bottom: ${theme.spacings.small};
+    }
+  `}
+`
+
+export const RegisterBox = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 ${theme.spacings.xlarge} ${theme.spacings.xlarge};
+
+    > span {
+      display: block;
+      margin: ${theme.spacings.xxsmall} 0;
+      font-size: ${theme.font.sizes.xsmall};
+    }
+  `}
+`
+
+export const CreateAccount = styled.a`
+  ${({ theme }) => css`
+    text-decoration: none;
+    color: ${theme.colors.primary};
+    border-bottom: 0.2rem solid ${theme.colors.primary};
+    cursor: pointer;
   `}
 `
