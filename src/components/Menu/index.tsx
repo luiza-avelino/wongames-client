98cb76c161f610d02d80282ae05a8112bcbb7d7d
Luiza Avelino <avelino.luiza98@gmail.com>
2022-04-27 13:39:20 -0300

    add menu accessibility & first tests

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -9,7 +9,7 @@ import * as S from './styles'
 const Menu = () => (
   <S.Wrapper>
     <S.IconWrapper>
-      <MenuIcon />
+      <MenuIcon aria-label="Open Menu" />
     </S.IconWrapper>
     <S.LogoWrapper>
       <Logo hideOnMobile />
@@ -17,10 +17,10 @@ const Menu = () => (
 
     <S.MenuGroup>
       <S.IconWrapper>
-        <SearchIcon />
+        <SearchIcon aria-label="Search" />
       </S.IconWrapper>
       <S.IconWrapper>
-        <ShoppingCartIcon />
+        <ShoppingCartIcon aria-label="open Shopping Cart" />
       </S.IconWrapper>
     </S.MenuGroup>
   </S.Wrapper>
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,9 +1,16 @@
-// import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
-// import Menu from '.'
+import Menu from '.'
 
 describe('<Menu />', () => {
-  it('should render the heading', () => {
-    expect(true).toBe(true)
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/Open Menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open Shopping Cart/i)).toBeInTheDocument()
+
+    expect(screen.getByLabelText(/Won Games/i)).toBeInTheDocument()
   })
 })
