4d7d4ef689b40901dc3d765f5b4e3cde05de6221
Luiza Avelino <avelino.luiza98@gmail.com>
2022-04-27 13:30:26 -0300

    initial menu component

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,9 @@
+// import { render, screen } from '@testing-library/react'
+
+// import Menu from '.'
+
+describe('<Menu />', () => {
+  it('should render the heading', () => {
+    expect(true).toBe(true)
+  })
+})
